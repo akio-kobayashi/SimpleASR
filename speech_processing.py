423841f17d05adc2cea22a48d93a8873bd441d10
akio-kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2021-05-18 16:47:27 +0900

    minor revision

--- speech_processing.py
+++ speech_processing.py
@@ -3,6 +3,6 @@ import numpy as np
 
 def compute_melspec(path):
     data, sr = librosa.load(path, sr=16000)
-    mel = np.log(librosa.feature.melspectrogram(y=data, sr=sr, n_mels=20, n_fft=512, win_length=400, hop_length=160))
+    mel = np.log(librosa.feature.melspectrogram(y=data, sr=sr, n_mels=40, n_fft=512, win_length=400, hop_length=160))
 
     return np.transpose(mel)
